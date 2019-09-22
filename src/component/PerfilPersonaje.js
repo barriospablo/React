d904aed9e3db27014926d8e54822c5b61698f8e9
Pablo Barrios <exepablo96@gmail.com>
2019-09-22 00:18:40 -0300

    Cambios en la card del detalle

--- src/component/PerfilPersonaje.js
+++ src/component/PerfilPersonaje.js
@@ -4,47 +4,61 @@ function PerfilPersonaje(props) {
   const { character } = props;
 
   return (
-    <div className="characterCard__Wrapper-sc-1ejywvi-0 lkWhZH">
-      <div
-        data="card header"
-        className="characterCard__ImgWrapper-sc-1ejywvi-1 eUyUcB"
-      >
-        <div className="card-image">
-          <img src={character.image} alt="Trandor" />
+    <div className="col-3 ml-auto mr-auto">
+      <div className="characterCard__Wrapper-sc-1ejywvi-0 lkWhZH">
+        <div
+          data="card header"
+          className="characterCard__ImgWrapper-sc-1ejywvi-1 eUyUcB"
+        >
+          <div className="card-image img-fluid">
+            <img src={character.image} alt="Trandor" />
+          </div>
+          <div className="characterCard__Title-sc-1ejywvi-3 dfWCUu">
+            <h2 className="characterCard__Name-sc-1ejywvi-4 ieUvkm">
+              {character.name}
+            </h2>
+            <p className="characterCard__Description-sc-1ejywvi-5 dSyDU">
+              id: 363 - created 2 years ago
+            </p>
+          </div>
         </div>
-        <div className="characterCard__Title-sc-1ejywvi-3 dfWCUu">
-          <h2 className="characterCard__Name-sc-1ejywvi-4 ieUvkm">
-            {character.name}
-          </h2>
-          <p className="characterCard__Description-sc-1ejywvi-5 dSyDU">
-            id: 363 - created 2 years ago
-          </p>
-        </div>
-      </div>
 
-      <div
-        data="card info"
-        className="characterCard__InfoWrapper-sc-1ejywvi-2 XgbIp"
-      >
-        <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
-          <span>STATUS</span>
-          <p>{character.status}</p>
-        </div>
-        <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
-          <span>SPECIES</span>
-          <p>{character.species}</p>
-        </div>
-        <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
-          <span>GENDER</span>
-          <p>{character.gender}</p>
-        </div>
-        <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
-          <span>ORIGIN</span>
-          <p>{character.origin ? character.origin.name : ""}</p>
-        </div>
-        <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
-          <span>LAST LOCATION</span>
-          <p>{character.location ? character.location.name : ""} </p>
+        <div
+          data="card info"
+          className="characterCard__InfoWrapper-sc-1ejywvi-2 XgbIp"
+        >
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
+                <span>STATUS</span>
+                <p>{character.status}</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
+                <span>SPECIES</span>
+                <p>{character.species}</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
+                <span>GENDER</span>
+                <p>{character.gender}</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
+                <span>ORIGIN</span>
+                <p>{character.origin ? character.origin.name : ""}</p>
+              </div>
+            </li>
+            <li className="list-group-item">
+              <div className="characterCard__TextWrapper-sc-1ejywvi-6 kYFLaT">
+                <span>LAST LOCATION</span>
+                <p>{character.location ? character.location.name : ""} </p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/component/Personaje.js
+++ src/component/Personaje.js
@@ -6,27 +6,29 @@ import { Link } from "react-router-dom";
 function Personaje(props) {
   const { character } = props;
   return (
-    <div id={character.key}>
-      <img src={character.image} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <Link
-          to={{
-            pathname: `/detalle/${character.id}`,
-            state: {
-              fromNotifications: true
-            }
-          }}
-        >
-          <div className="CharacterCard__name-container text-truncate">
-            {character.name}
-          </div>
-        </Link>
+    <div className="card my-4">
+      <div id={character.key}>
+        <img src={character.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <Link
+            to={{
+              pathname: `/detalle/${character.id}`,
+              state: {
+                fromNotifications: true
+              }
+            }}
+          >
+            <div className="CharacterCard__name-container text-truncate">
+              {character.name}
+            </div>
+          </Link>
 
-        <p className="card-text">{character.especie}</p>
-        <p className="card-text">
-          <small className="text-muted">{character.origen}</small>
-        </p>
-        <p>{character.genero}</p>
+          <p className="card-text">{character.especie}</p>
+          <p className="card-text">
+            <small className="text-muted">{character.origen}</small>
+          </p>
+          <p>{character.genero}</p>
+        </div>
       </div>
     </div>
   );
